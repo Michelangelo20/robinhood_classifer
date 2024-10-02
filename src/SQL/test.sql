d3a7d1148fd1861a932c02809582ed141b0ae4eb
Eric Cheng <cheng.eric11@gmail.com>
2024-10-02 16:02:13 -0400

    get min and max dates of users to compare with churn activity

---
+++ src/SQL/test.sql
@@ -0,0 +1,36 @@
+WITH temp1 AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY timestamp) AS rn,
+        timestamp::date - ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY timestamp)::int AS streak_id
+    FROM
+        equity_value_data
+),
+temp2 AS (
+    SELECT
+        user_id,
+        MIN(timestamp::date) AS min_date,
+        MAX(timestamp::date) AS max_date,
+        COUNT(*) AS duration_of_above10_streak
+    FROM
+        temp1
+    GROUP BY
+        user_id, streak_id
+),
+temp3 AS (
+    SELECT
+        *,
+        LAG(max_date) OVER (PARTITION BY user_id ORDER BY min_date ASC) AS last_streak_date,
+        min_date - LAG(max_date) OVER (PARTITION BY user_id ORDER BY min_date ASC) AS duration_between_above10_streaks
+    FROM
+        temp2
+),
+temp4 AS (
+    SELECT *
+    FROM temp3
+    WHERE duration_between_above10_streaks >= 28
+),
+temp5 as (
+    select distinct user_id from temp4
+)
+select * from temp4
